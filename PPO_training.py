a37368c329660a94e2327d7b4bcc5677ccb10dcb
seblessa <sebastiaolessa2002@outlook.com>
2023-12-05 14:41:24 +0000

    working

---
+++ PPO_training.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import MlpPolicy
+import os
+
+models_dir = "models/PPO"
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+env = gym.make('CarRacing-v2')
+env.reset()
+
+model = PPO(MlpPolicy, env, verbose=1)
+
+TIMESTEPS = 10000
+iters = 0
+while True:
+    iters += 1
+
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
+    model.save(f"{models_dir}/{TIMESTEPS * iters}")
--- agent_training.py
+++
@@ -1,5 +0,0 @@
-import gym
-from stable_baselines3 import PPO
-from car_racing import CarRacing
-
-
