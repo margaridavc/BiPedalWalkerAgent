9c0e08f7f1c0ea5e7d4ffbd8333cfd5c3649a358
seblessa <sebastiaolessa2002@outlook.com>
2023-12-07 02:20:56 +0000

    Added a way to human test the environment

---
+++ human_test.py
@@ -0,0 +1,62 @@
+import numpy as np
+import gymnasium as gym
+from NewReward import RewardWrapper
+import pygame
+
+a = np.array([0.0, 0.0, 0.0])
+
+
+def register_input():
+    global quit, restart
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                a[0] = -1.0
+            if event.key == pygame.K_RIGHT:
+                a[0] = +1.0
+            if event.key == pygame.K_UP:
+                a[1] = +1.0
+            if event.key == pygame.K_DOWN:
+                a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
+            if event.key == pygame.K_RETURN:
+                restart = True
+            if event.key == pygame.K_ESCAPE:
+                quit = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                a[0] = 0
+            if event.key == pygame.K_RIGHT:
+                a[0] = 0
+            if event.key == pygame.K_UP:
+                a[1] = 0
+            if event.key == pygame.K_DOWN:
+                a[2] = 0
+
+        if event.type == pygame.QUIT:
+            quit = True
+
+
+# env = RewardWrapper(gym.make('CarRacing-v2', render_mode="human"))
+env = gym.make('CarRacing-v2', render_mode="human")
+
+quit = False
+# while not quit:
+env.reset()
+total_reward = 0.0
+steps = 0
+restart = False
+while True:
+    register_input()
+    obs, reward, terminated, truncated, info = env.step(a)
+    total_reward += reward
+    if terminated:
+        print("terminated")
+        exit(0)
+    if steps % 200 == 0 or truncated:
+        print("\naction " + str([f"{x:+0.2f}" for x in a]))
+        print(f"step {steps} total_reward {total_reward:+0.2f}")
+    steps += 1
+    if terminated or truncated or restart or quit:
+        break
+env.close()
