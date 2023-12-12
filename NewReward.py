741cd6fe7ec327da6adc82df22d1449716d69e6d
seblessa <sebastiaolessa2002@outlook.com>
2023-12-12 15:43:47 +0000

    Training more models

--- NewReward.py
+++ NewReward.py
@@ -4,19 +4,23 @@ from gymnasium import RewardWrapper as RW
 class RewardWrapper(RW):
     def __init__(self, env):
         super().__init__(env)
-        self.total_reward = 25
+        self.env = env
+        self.penalty_limit = 500
 
     def reset(self, **kwargs):
-        self.total_reward = 0
+        self.penalty_limit = 500
         return self.env.reset(**kwargs)
 
     def step(self, action):
         # Perform the environment step
         obs, reward, done, _, info = self.env.step(action)
 
-        self.total_reward += reward
-
-        if self.total_reward < 0:
-            done = True
+        # print(f"Penalty limit: {self.penalty_limit}")
+        if float(reward) < 0:
+            self.penalty_limit -= 1
+            if self.penalty_limit == 0:
+                done = True
+        else:
+            self.penalty_limit = 500
 
         return obs, reward, done, _, info
--- training.py
+++ training.py
@@ -1,22 +1,19 @@
 import os
 from sys import argv
-from stable_baselines3 import A2C, DDPG, HER, PPO, SAC, TD3
+from stable_baselines3 import A2C, DDPG, PPO, SAC, TD3
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.a2c import MlpPolicy as A2C_MlpPolicy
 from stable_baselines3.ddpg import MlpPolicy as DDPG_MlpPolicy
-# from stable_baselines3.her import MlpPolicy as HER_MlpPolicy
 from stable_baselines3.ppo import MlpPolicy as PPO_MlpPolicy
 from stable_baselines3.sac import MlpPolicy as SAC_MlpPolicy
 from stable_baselines3.td3 import MlpPolicy as TD3_MlpPolicy
-
 from NewReward import RewardWrapper
 
 env_id = "CarRacing-v2"
-TIMESTEPS = 100000
+TIMESTEPS = 10000
 models_dir = "models"
 logdir = "logs"
-NUM_ENVS = os.cpu_count()
 
 
 def latest_model(algorithm):
@@ -25,7 +22,10 @@ def latest_model(algorithm):
     return f"{models_dir}/{algorithm}/{models[-1]}.zip"
 
 
-def train_model(algo, algo_name, policy, env):
+def train_model(algo, algo_name, policy, n_envs=os.cpu_count()):
+    env = make_vec_env(env_id, n_envs=n_envs, vec_env_cls=SubprocVecEnv, wrapper_class=RewardWrapper,
+                       vec_env_kwargs=dict(start_method='fork'))
+
     if os.path.exists(f"{models_dir}/{algo_name}"):
         if os.listdir(f"{models_dir}/{algo_name}"):
             model_path = latest_model(algo_name)
@@ -36,12 +36,12 @@ def train_model(algo, algo_name, policy, env):
             # policy_kwargs=dict(net_arch=[{"conv1d": [32, 3, 1]}, {"fc": [256]}])  CNN nn architecture
 
             model = algo(policy, env, verbose=1,
-                         tensorboard_log=logdir)  # (hyperparameter) ,learning_rate=0.0001, (Neural Network Architecture change)(Neural Network Architecture change) policy_kwargs=dict(net_arch=[256,(...n_layers...), 256]))
+                         tensorboard_log=logdir)  # (hyperparameter), learning_rate=0.0001, (Neural Network Architecture change) (Neural Network Architecture change) policy_kwargs=dict(net_arch=[256,(...n_layers...), 256]))
             iters = 0
     else:
         os.makedirs(f"{models_dir}/{algo_name}")
         model = algo(policy, env, verbose=1,
-                     tensorboard_log=logdir)  # (hyperparameter) ,learning_rate=0.0001, (Neural Network Architecture change) policy_kwargs=dict(net_arch=[256,(...n_layers...), 256])
+                     tensorboard_log=logdir)  # (hyperparameter), learning_rate=0.0001, (Neural Network Architecture change) policy_kwargs=dict(net_arch=[256,(...n_layers...), 256])
         iters = 0
 
     while True:
@@ -61,22 +61,16 @@ def main():
 
         model_type = argv[1]
 
-        # Create the vectorized environment using make_vec_env and the new wrapper: RewardWrapper
-        env = make_vec_env(env_id, n_envs=NUM_ENVS, vec_env_cls=SubprocVecEnv,
-                           vec_env_kwargs=dict(start_method='fork'))
-
         if model_type == "A2C":
-            train_model(A2C, model_type, A2C_MlpPolicy, env)
+            train_model(A2C, model_type, A2C_MlpPolicy)
         elif model_type == "DDPG":
-            train_model(DDPG, model_type, DDPG_MlpPolicy, env)
-        # elif model_type == "HER":
-            # train_model(HER, model_type, HER_MlpPolicy, env)
+            train_model(DDPG, model_type, DDPG_MlpPolicy, n_envs=1)
         elif model_type == "PPO":
-            train_model(PPO, model_type, PPO_MlpPolicy, env)
+            train_model(PPO, model_type, PPO_MlpPolicy)
         elif model_type == "SAC":
-            train_model(SAC, model_type, SAC_MlpPolicy, env)
+            train_model(SAC, model_type, SAC_MlpPolicy)
         elif model_type == "TD3":
-            train_model(TD3, model_type, TD3_MlpPolicy, env)
+            train_model(TD3, model_type, TD3_MlpPolicy, n_envs=1)
         else:
             raise ValueError("Invalid argument. Please specify a valid algorithm.")
 
